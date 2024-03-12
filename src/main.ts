e380891b321244fb29596e5e29c779aba2832f59
mrpupi <miromannz@mail.ru>
2024-03-12 15:31:32 +0300

    medium-rare

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
-import './dist/style.css'
+import "./dist/style.css";
 
+// Получаем элементы инпутов
+const input1 = document.getElementById("r1") as HTMLInputElement;
+const input2 = document.getElementById("r2") as HTMLInputElement;
+const output = document.getElementById("documento") as HTMLInputElement;
+
+if (input1 && input2) {
+  input1.addEventListener("change", updateOutput);
+  input2.addEventListener("change", updateOutput);
+}
+
+  function updateOutput() {
+    if (output && input1 && input2) {
+        if(input1.checked) {
+          output.placeholder = "DNI o Tarjeta de residencia"
+        } else if (input2.checked) {
+          output.placeholder = "Pasaporte"
+        }
+      }
+    }
+  
+    // (input1.value === "val1")
+    // (input2.value === "val2")
